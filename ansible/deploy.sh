809f56760987246c0de71e79b78eb21f3b89efec
dalvarezh <dalvarezh@indra.es>
2023-03-18 11:00:42 +0100

    Add login to the k8s cluster in the ansible script

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -13,5 +13,6 @@ cd $ANSIBLE_PATH
 sed -i "8s/.*/REGISTRY_PASSWORD: $PASSWORD/" vars/webserver_vars.yaml
 sed -i "3s/.*/  hosts: $PUBLIC_IP/" playbook.yaml
 echo $PUBLIC_IP >> hosts
+az aks get-credentials --resource-group rg-dalvarezh --name k8s
 ansible-playbook -i hosts playbook.yaml --key-file ~/.ssh/id_rsa
 ansible-playbook -i hosts k8s_playbook.yaml --key-file ~/.ssh/id_rsa
